744d082f9ea45b2781d86744fe1a51cf44870c5d
fwar34 <fwar34@126.com>
2023-04-05 23:01:24 +0800

    update

--- JavaBean/src/com/feng/Main.java
+++ JavaBean/src/com/feng/Main.java
@@ -3,10 +3,15 @@ package com.feng;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+
 public class Main {
     static Log log = LogFactory.getLog(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IntrospectionException {
         Person person = new Person();
         person.setName("XiaoMing");
         log.info(person.hello());
@@ -14,6 +19,13 @@ public class Main {
             new Person();
         } catch (Exception e) {
             log.error("Exception", e);
+        }
+
+        BeanInfo beanInfo = Introspector.getBeanInfo(Person.class);
+        for (PropertyDescriptor descriptor : beanInfo.getPropertyDescriptors()) {
+            System.out.println(descriptor.getName());
+            System.out.println(" " + descriptor.getReadMethod());
+            System.out.println(" " + descriptor.getWriteMethod());
         }
    }
 }
